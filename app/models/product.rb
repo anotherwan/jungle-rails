22bacd40a20efd56f906cf5d3109189d765d311c
anotherwan <itsanotherwan@gmail.com>
2017-02-17 17:58:09 -0500

    removed ', -> { order(created_at: :desc) },' and added validations and relationships

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,10 +4,15 @@ class Product < ActiveRecord::Base
   mount_uploader :image, ProductImageUploader
 
   belongs_to :category
+  has_many :reviews, dependent: :destroy
+  # , -> { order(created_at: :desc) }, 
 
   validates :name, presence: true
   validates :price, presence: true
   validates :quantity, presence: true
   validates :category, presence: true
 
+  def average_rating
+    self.reviews.average(:rating)
+  end
 end
